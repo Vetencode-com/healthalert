29bfb5d34daf41db9d45442513e1a7eb57d7c535
WildanMZaki <wildanmzaki7@gmail.com>
2024-10-28 09:10:19 +0700

    feat: initial model and migrations

---
+++ database/migrations/2024_10_28_015236_create_prescription_medicines_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prescription_medicines', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('prescription_id')->constrained('prescriptions')->onDelete('cascade');
+            $table->foreignId('medicine_id')->constrained('medicines')->onDelete('cascade');
+            $table->unsignedInteger('frequency_per_day')->default(1);
+            $table->time('end_time')->nullable(); // E.g., when reminders should stop
+            $table->unsignedInteger('duration_days')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prescription_medicines');
+    }
+};
---
+++ database/migrations/2024_10_28_015250_create_prescription_medicine_times_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prescription_medicine_times', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('prescription_medicine_id')->constrained('prescription_medicines')->onDelete('cascade');
+            $table->time('time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prescription_medicine_times');
+    }
+};
