3b23e2cdb6b99fe84cf92ffce3d84c34cb04451a
WildanMZaki <wildanmzaki7@gmail.com>
2024-10-30 11:06:37 +0700

    fix: change dummy text in create prescription form

--- resources/views/prescriptions/create.blade.php
+++ resources/views/prescriptions/create.blade.php
@@ -11,7 +11,7 @@
             <div class="row">
                 <div class="col-12 col-md-6 order-md-1 order-last">
                     <h3>Buat Resep</h3>
-                    <p class="text-subtitle text-muted">The default layout.</p>
+                    <p class="text-subtitle text-muted">Buat peresepan obat baru</p>
                 </div>
 
             </div>
